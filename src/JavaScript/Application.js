6ebdb051df7b486f58ca67a9f4e5492d9aa40a8d
Jérémie Roulin <jeremie.roulin@heig-vd.ch>
2016-09-21 22:52:16 +0200

    [BUGFIX] UpperCamelCase attribs were transformed toLower

--- src/JavaScript/Application.js
+++ src/JavaScript/Application.js
@@ -1,4 +1,5 @@
 import AbstractApplication from "./AbstractApplication";
+import * as HTML5Tokenizer from "../Vendors/simple-html-tokenizer/lib/simple-html-tokenizer/index";
 
 export default class Application extends AbstractApplication {
 	constructor() {
@@ -6,8 +7,33 @@ export default class Application extends AbstractApplication {
 	}
 
 	start() {
+		Application.initializeConvertForm()
 	}
 
 	initializePlugins() {
+	}
+
+	static initializeConvertForm() {
+		$('#conversion-form').submit((event) => {
+			event.preventDefault();
+			Application.convertFluidTags();
+		});
+	}
+
+	static convertFluidTags() {
+		var textToConvert = $('#fluid-tag-to-convert').val();
+		var viewHelperName = textToConvert.split(/(<(.+?) )/)[2];
+		var attrs = HTML5Tokenizer.tokenize(textToConvert)[0].attributes;
+		var textConverted = '{' + viewHelperName + '(';
+		$.each(attrs, (i, attr) => {
+			textConverted += attr[0] + ': \'' + attr[1];
+			if (i < attrs.length - 1) {
+				textConverted += '\', ';
+			} else {
+				textConverted += '\'';
+			}
+		});
+		textConverted += ')}';
+		$('#conversion-result').html(textConverted).removeClass('hide');
 	}
 }
--- src/JavaScript/Main.js
+++ src/JavaScript/Main.js
@@ -3,22 +3,3 @@ import Application from "./Application";
 let app = new Application();
 app.initializePlugins();
 app.start();
-
-$('#conversion-form').submit(function (e) {
-    e.preventDefault();
-    var textToConvert = $('#textToConvert').val();
-    var viewHelperName = textToConvert.split(/(<(.+?) )/)[2];
-    var total = $(textToConvert)[0].attributes.length;
-    var textConverted = '{' + viewHelperName + '(';
-    $.each($(textToConvert)[0].attributes, function (i, attrib) {
-        var attribName = attrib.name;
-        var value = attrib.value;
-        if (i < total - 1) {
-            textConverted += attribName + ': \'' + value + '\', ';
-        } else {
-            textConverted += attribName + ': \'' + value + '\'';
-        }
-    });
-    textConverted += ')}';
-    $('#conversionResult').html(textConverted).removeClass('hide');
-});
