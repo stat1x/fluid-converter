1f27fdd961121e95fd6635170ab814eaa9df798a
Jérémie Roulin <jeremie.roulin@heig-vd.ch>
2016-10-02 15:23:40 +0200

    [TASK] Add sourcemaps

--- gulp/sass.js
+++ gulp/sass.js
@@ -5,11 +5,13 @@ import autoprefixer from "gulp-autoprefixer";
 import rename from "gulp-rename";
 import scssLint from "gulp-scss-lint";
 import cleanCss from "gulp-clean-css";
+import sourceMaps from "gulp-sourcemaps";
 import config from "../gulp.config";
 
 gulp.task('sass', () => {
 	return gulp.src(config.src.stylesheets + '**/*.scss')
 		.pipe(plumber())
+		.pipe(sourceMaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: config.COMPATIBILITY
@@ -18,6 +20,7 @@ gulp.task('sass', () => {
 			suffix: '.min'
 		}))
 		.pipe(cleanCss())
+		.pipe(sourceMaps.write())
 		.pipe(gulp.dest(config.dist.stylesheets))
 		.on('finish', () => {
 			gulp.src(config.src.stylesheets + '**/*.scss').pipe(scssLint({
