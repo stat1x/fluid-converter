f5215895ae800cdcfd406ac07f3f4bcc97cc17af
Jérémie Roulin <jeremie.roulin@heig-vd.ch>
2016-11-05 17:43:43 +0100

    [TASK] Add test

--- tests/checkStructure.js
+++ tests/checkStructure.js
@@ -1,7 +1,9 @@
 describe('getDiv', function() {
-	var d = document.querySelector('#fluid-node-to-convert');
+	var d = document.querySelector('#fluid-node-to-convert'),
+		e = document.querySelector('#fluid-node-error');
 
 	it('Should exist', function() {
 		expect(d.nodeName).toBe('TEXTAREA');
+		expect(e.nodeName).toBe('P');
 	});
 });
