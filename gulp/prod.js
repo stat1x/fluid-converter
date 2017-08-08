b8fdcedbf2d051d619994f7b58b8ab19b5a8b1ef
Jérémie Roulin <jeremie@hemmer.ch>
2017-08-08 08:37:17 +0200

    Test

--- gulp/prod.js
+++ gulp/prod.js
@@ -1,13 +1,4 @@
 import gulp from 'gulp';
-import browser from 'browser-sync';
-import config from '../gulp.config';
-
-gulp.task('serve', () => {
-    browser.init({
-        server: config.distPath,
-        open: false
-    });
-});
 
 gulp.task('heroku:production', () => {
 	gulp.start('javascript', 'images', 'template', 'sass');
